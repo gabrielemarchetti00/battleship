a5adadbfc144add5f2466f729881b961ccc3d987
gabrielemarchetti00 <gabriele.marchetti26@gmail.com>
2023-04-23 16:34:37 +0200

    add gamecontroller and displaycontroller

--- src/index.js
+++ src/index.js
@@ -1,9 +1,111 @@
-export { Ship }
+export {Ship, Gameboard, Player}
 
-const Ship = (length, hits, sunk) => {
-    const hit = () => hits++
-    const isSunk = (length, hits) => {
-        if(length == hits) sunk = true
+const Ship = (length, hits = 0, sunk = false) => {
+    const hit = function() {
+        this.hits ++;
+        this.isSunk();
+    }
+    const isSunk = function() {
+        if(this.length == this.hits) {
+            this.sunk = true;
+        }
     }
     return {length, hits, sunk, hit, isSunk}
 }
+
+const Gameboard = () => {
+    const board = [ [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null], ]
+    const place = function(x, y, ship) {
+        this.board[x][y] = ship
+    }
+    const receiveAttack = function(x, y) {
+        if(this.board[x][y] != null) {
+            board[x][y].hit();
+        }
+        else {
+            board[x][y] = 'X';
+        }
+    }
+    const allSunk = function() {
+        let allsunk = true;
+        for(let i=0; i<this.board.length; i++){
+            for(let j=0; j<this.board[i].length; j++){
+                if(board[i][j] != null && board[i][j].sunk == false){
+                    allsunk = false;
+                }
+            }
+        }
+        return allsunk;
+    }
+    return {board, place, receiveAttack, allSunk}
+}
+
+const Player = () => {
+    const attack = function(x, y, gameboard) {
+        gameboard.receiveAttack(x, y);
+    }
+    return {attack}
+}
+
+const gameController = (() => {
+    const player = Player();
+    const computer = Player();
+
+    const playerBoard = Gameboard();
+    let playerGrid = document.querySelector('.player-board');
+    for(let i=0; i<playerBoard.board.length; i++){
+        for(let j=0; j<playerBoard.board[i].length; j++){
+            let cell = document.createElement('div');
+            cell.id = `${i}${j}`;
+            playerGrid.appendChild(cell);
+        }
+    }
+
+    const computerBoard = Gameboard();
+    let computerGrid = document.querySelector('.computer-board');
+    for(let i=0; i<computerBoard.board.length; i++){
+        for(let j=0; j<computerBoard.board[i].length; j++){
+            let cell = document.createElement('div');
+            cell.id = `${i}${j}`;
+            computerGrid.appendChild(cell);
+        }
+    }
+
+    let activePlayer = player;
+    const getActivePlayer = () => activePlayer;
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === player ? computer : player;
+    };
+
+    const playRound = () => {
+        switchPlayerTurn();
+    };
+
+    let winner;
+    const checkWin = () => {
+    }
+
+    return {playRound, getActivePlayer, checkWin};
+})();
+
+const displayController = (() => {
+    const updateScreen = () => {
+        const activePlayer = gameController.getActivePlayer();
+    }
+
+    function clickHandlerBoard(){
+        updateScreen();
+    };
+
+    return{updateScreen}
+})();
--- src/test.js
+++ src/test.js
@@ -1,4 +1,4 @@
-import { Ship } from "./index"
+import {Ship, Gameboard, Player} from "./index"
 
 test('ship objects creation', () => {
     const ship = Ship(3, 0, false)
@@ -6,7 +6,65 @@ test('ship objects creation', () => {
 })
 
 test('hit function', () => {
-    const ship = Ship(3, 0, false)
+    const ship = Ship(3)
     ship.hit();
     expect(ship.hits).toBe(1);
+})
+
+test('isSunk function', () => {
+    const ship = Ship(2);
+    ship.hit();
+    ship.hit();
+    ship.isSunk();
+    expect(ship.sunk).toBe(true);
+})
+
+test('place function', () => {
+    const gameboard = Gameboard();
+    const ship = Ship(1);
+    gameboard.place(5,5,ship);
+    expect(gameboard.board[5][5]).toBe(ship);
+})
+
+test('receive attack function (hit)', () => {
+    const gameboard = Gameboard();
+    const ship = Ship(1);
+    gameboard.place(5,5,ship);
+    gameboard.receiveAttack(5,5);
+    expect(gameboard.board[5][5].hits).toBe(1);
+})
+
+test('receive attack function (miss)', () => {
+    const gameboard = Gameboard();
+    const ship = Ship(1);
+    gameboard.place(5,5,ship);
+    gameboard.receiveAttack(5,6);
+    expect(gameboard.board[5][6]).toBe('X');
+})
+
+test('allsunk', () => {
+    const gameboard = Gameboard();
+    const ship1 = Ship(1);
+    const ship2 = Ship(2);
+
+    gameboard.place(5,5,ship1);
+    gameboard.place(1,1,ship2);
+    gameboard.place(1,2,ship2);
+
+    gameboard.receiveAttack(5,5);
+    gameboard.receiveAttack(1,1);
+    gameboard.receiveAttack(1,2);
+
+    expect(gameboard.allSunk()).toBe(true);
+})
+
+test('attack by player', () => {
+    const gameboard = Gameboard();
+    const player = Player();
+    const ship1 = Ship(1);
+    gameboard.place(1,1,ship1);
+
+    player.attack(1,1,gameboard);
+    expect(gameboard.board[1][1].hits).toBe(1);
+
 })
