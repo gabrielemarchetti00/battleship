09c189820ee6ecd81ca16b3e821671b019a2327a
gabrielemarchetti00 <gabriele.marchetti26@gmail.com>
2023-04-24 11:37:39 +0200

    place ships and color when clicked

--- src/index.js
+++ src/index.js
@@ -56,29 +56,105 @@ const Player = () => {
     return {attack}
 }
 
-const gameController = (() => {
-    const player = Player();
-    const computer = Player();
-
-    const playerBoard = Gameboard();
+function createBoards(pb, cb) {
     let playerGrid = document.querySelector('.player-board');
-    for(let i=0; i<playerBoard.board.length; i++){
-        for(let j=0; j<playerBoard.board[i].length; j++){
+    for(let i=0; i<pb.board.length; i++){
+        for(let j=0; j<pb.board[i].length; j++){
             let cell = document.createElement('div');
+            cell.className = 'pcell';
             cell.id = `${i}${j}`;
             playerGrid.appendChild(cell);
         }
     }
 
-    const computerBoard = Gameboard();
     let computerGrid = document.querySelector('.computer-board');
-    for(let i=0; i<computerBoard.board.length; i++){
-        for(let j=0; j<computerBoard.board[i].length; j++){
+    for(let i=0; i<cb.board.length; i++){
+        for(let j=0; j<cb.board[i].length; j++){
             let cell = document.createElement('div');
+            cell.className = 'ccell';
             cell.id = `${i}${j}`;
             computerGrid.appendChild(cell);
         }
     }
+}
+
+function placeShips(pb, cb) {
+    const ship1p = Ship(1);
+    const ship2p = Ship(2);
+    pb.place(1,1,ship1p);
+    pb.place(5,5,ship2p);
+    pb.place(5,6,ship2p);
+
+    const ship1c = Ship(1);
+    const ship2c = Ship(2);
+    cb.place(0,1,ship1c);
+    cb.place(8,5,ship2c);
+    cb.place(7,5,ship2c);
+}
+
+function colorCell(){
+    const playercells = document.querySelectorAll('.pcell');
+    playercells.forEach((cell) => {
+        for(let i=0; i<playerBoard.board.length; i++){
+            for(let j=0; j<playerBoard.board[i].length; j++){
+                if(cell.id == `${i}${j}` && playerBoard.board[i][j] != null) {
+                    cell.style.backgroundColor = 'red';
+                }
+            }
+        }
+    })
+
+    const computercells = document.querySelectorAll('.ccell');
+    computercells.forEach((cell) => {
+        for(let i=0; i<computerBoard.board.length; i++){
+            for(let j=0; j<computerBoard.board[i].length; j++){
+                if(cell.id == `${i}${j}` && computerBoard.board[i][j] != null) {
+                    cell.style.backgroundColor = 'blue';
+                }
+            }
+        }
+    })
+}
+
+
+const playerBoard = Gameboard();
+const computerBoard = Gameboard();
+
+
+const displayController = (() => {
+    const updateScreen = () => {
+        colorCell();
+    }
+
+    function clickHandlerBoard(){
+        gameController.playRound();
+        updateScreen();
+    };
+
+    return{updateScreen}
+})();
+
+const gameController = (() => {
+    const player = Player();
+    const computer = Player();
+
+    createBoards(playerBoard, computerBoard);
+    placeShips(playerBoard, computerBoard);
+
+    displayController.updateScreen();
+
+    const computercells = document.querySelectorAll('.ccell');
+    computercells.forEach((cell) => {
+        cell.addEventListener('click', () => {
+            for(let i=0; i<computerBoard.board.length; i++){
+                for(let j=0; j<computerBoard.board[i].length; j++){
+                    if(cell.id == `${i}${j}` && computerBoard.board[i][j] != null) {
+                        cell.style.backgroundColor = 'lightBlue';
+                    }
+                }
+            }
+        })
+    })
 
     let activePlayer = player;
     const getActivePlayer = () => activePlayer;
@@ -88,6 +164,7 @@ const gameController = (() => {
     };
 
     const playRound = () => {
+        console.log('cioa');
         switchPlayerTurn();
     };
 
@@ -98,14 +175,3 @@ const gameController = (() => {
     return {playRound, getActivePlayer, checkWin};
 })();
 
-const displayController = (() => {
-    const updateScreen = () => {
-        const activePlayer = gameController.getActivePlayer();
-    }
-
-    function clickHandlerBoard(){
-        updateScreen();
-    };
-
-    return{updateScreen}
-})();
